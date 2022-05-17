ffdb03a04b9e997fd67f81445cdf3bf91ac6a78e
vaisakhg0 <vaisakhg30@gmail.com>
2022-05-17 10:29:40 +0530

    search

--- src/components/Addstudent.js
+++ src/components/Addstudent.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Addstudent = () => {
   return (
-    <div className="container">
+    <div><div className="container">
     <div className="row">
         <div className="col">
             <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div className="row">
+            <div className="row g-3">
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Name</label>
                     <input type="text" className="form-control"/>
@@ -33,7 +33,7 @@ const Addstudent = () => {
             </div>
         </div>
     </div>
-</div></div>
+</div></div></div>
   )
 }
 
--- src/components/Searchstudent.js
+++ src/components/Searchstudent.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 const Searchstudent = () => {
   return (
-    <div><div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div>
+        <div className="container">
+        <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12 col-xl-12 col-xxl-12">
     <label for="" className="form-label">University reg no</label>
     <input type="text" className="form-control"/>
 </div>
-<div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12 col-xl-12 col-xxl-12">
+<div className="col col-12 col-sm-12 col-md-12 col-lg-12  col-xl-12 col-xxl-12">
     <button className="btn btn-success">Search</button>
+</div></div>
 </div></div>
   )
 }
