4aaa895924302c8ab20baa56de1c60f1f0287242
vaisakhg0 <vaisakhg30@gmail.com>
2022-05-17 10:00:04 +0530

    search

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Addstudent from './components/Addstudent';
+import Searchstudent from './components/Searchstudent';
 
 function App() {
   return (
-    <Addstudent/>
+    <div>
+      <Addstudent/>
+      <Searchstudent/>
+    </div>
   );
 }
 
---
+++ src/components/Searchstudent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Searchstudent = () => {
+  return (
+    <div><div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12 col-xl-12 col-xxl-12">
+    <label for="" className="form-label">University reg no</label>
+    <input type="text" className="form-control"/>
+</div>
+<div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12 col-xl-12 col-xxl-12">
+    <button className="btn btn-success">Search</button>
+</div></div>
+  )
+}
+
+export default Searchstudent
